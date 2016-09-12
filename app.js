b5d528e63b31a76e351b79ae5ef4729ebd6d4238
Juraci Vieira <juraci.vieira@gmail.com>
2016-09-12 10:11:00 -0300

    spike sequelize

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 import express from 'express';
+import Sequelize from 'sequelize';
 
 const app = express();
+
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+let Goal = sequelize.define('goal', {
+  description: {
+    type: Sequelize.STRING,
+    field: 'description'
+  }
+}, {
+  freezeTableName: true
+});
+
+Goal.sync({force: true}).then(() => {
+  return Goal.create({
+    description: 'Lear sequelize'
+  });
+});
+
+app.route('/')
+  .get((req, res) => {
+    Goal.findOne().then((goal) => {
+      res.status(200).json(goal);
+    });
+  });
 
 export default app;
