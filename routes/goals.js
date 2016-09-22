512991a1bc8a0a7f8e45c200a6c2e806dc82a429
Juraci Vieira <juraci.vieira@gmail.com>
2016-09-21 23:58:35 -0300

    POST /goals

--- routes/goals.js
+++ routes/goals.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const router = express.Router();
+const jsonParser = bodyParser.json();
 
 export default (datasource) => {
+  const Goal = datasource.models.Goal;
+
   router.route('/')
     .get((req, res) => {
-      datasource.models.Goal.findAll()
+      Goal.findAll()
         .then((goals) => {
           let goalsObj = { data: [] };
           goals.forEach((goal) => {
@@ -23,6 +27,16 @@ export default (datasource) => {
         })
         .catch((err) => {
           res.sendStatus(404);
+        });
+    })
+    .post(jsonParser, (req, res) => {
+      let goal = req.body.data;
+      Goal.create({ description: goal.attributes.description })
+        .then(() => {
+          res.sendStatus(204);
+        })
+        .catch((err) => {
+          console.log(err);
         });
     });
 
--- test/integration/goals_spec.js
+++ test/integration/goals_spec.js
@@ -5,12 +5,12 @@ describe('goals', function() {
     beforeEach((done) => {
       app.datasource.sequelize.sync().then(() => {
         Goal
-        .destroy({ where: {} })
-        .then(() => {
-          return Goal.create({ description: 'Learn Node.js' });
-        }).then(() => {
-          done();
-        });
+          .destroy({ where: {} })
+          .then(() => {
+            return Goal.create({ description: 'Feel comfortable with Node.js' });
+          }).then(() => {
+            done();
+          });
       });
     });
 
@@ -20,7 +20,36 @@ describe('goals', function() {
         .end((err, res) => {
           expect(res.status).to.equal(200);
           expect(res.body.data).to.have.length(1);
-          expect(res.body.data[0].attributes.description).to.equal('Learn Node.js');
+          expect(res.body.data[0].attributes.description).to.equal('Feel comfortable with Node.js');
+          done(err);
+        });
+    });
+  });
+
+  describe('POST /goals', function() {
+    const goal = {
+      data: {
+        type: 'goals',
+        attributes: {
+          description: 'Feel comfortable with CSS'
+        }
+      }
+    }
+
+    beforeEach((done) => {
+      Goal
+        .destroy({ where: {} })
+        .then(() => {
+          done();
+        });
+    });
+
+    it('creates a new goal', function(done) {
+      request
+        .post('/goals')
+        .send(goal)
+        .end((err, res) => {
+          expect(res.status).to.equal(204);
           done(err);
         });
     });
