7c8a9a37626dc45df10b0592534d31f45161f90e
Juraci Vieira <juraci.vieira@gmail.com>
2016-10-05 17:54:33 -0300

    Extracting datasource operations to controllers

---
+++ controllers/goals.js
@@ -0,0 +1,46 @@
+class GoalsController {
+  constructor(Goal) {
+    this.Goal = Goal;
+  }
+
+  findAll() {
+    return this.Goal.findAll()
+      .then((goals) => {
+        let goalsObj = { data: [] };
+        goals.forEach((goal) => {
+          goalsObj.data.push(
+            {
+              type: 'goals',
+              id: goal.id,
+              attributes: {
+                description: goal.description
+              }
+            }
+          );
+        });
+        return {
+          data: goalsObj,
+          status: 200
+        };
+      })
+      .catch((err) => {
+        return {
+          data: err,
+          sattus: 404
+        }
+      });
+  }
+
+  create(goal) {
+    return this.Goal.create(goal)
+      .then(() => {
+        return { status: 204 };
+      })
+      .catch((err) => {
+        return { data: err, status: 400 };
+      });
+  }
+
+}
+
+export default GoalsController;
--- routes/goals.js
+++ routes/goals.js
@@ -1,42 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import GoalsController from '../controllers/goals';
 
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
 export default (datasource) => {
   const Goal = datasource.models.Goal;
+  const goalsController = new GoalsController(Goal);
 
   router.route('/')
     .get((req, res) => {
-      Goal.findAll()
-        .then((goals) => {
-          let goalsObj = { data: [] };
-          goals.forEach((goal) => {
-            goalsObj.data.push(
-              {
-                type: 'goals',
-                id: goal.id,
-                attributes: {
-                  description: goal.description
-                }
-              }
-            );
-          });
-          res.status(200).json(goalsObj);
-        })
-        .catch((err) => {
-          res.sendStatus(404);
+      goalsController.findAll()
+        .then((result) => {
+          res.status(result.status).json(result.data);
         });
     })
     .post(jsonParser, (req, res) => {
-      let goal = req.body.data;
-      Goal.create({ description: goal.attributes.description })
-        .then(() => {
-          res.sendStatus(204);
-        })
-        .catch((err) => {
-          console.log(err);
+      let goal = req.body.data.attributes;
+      goalsController.create(goal)
+        .then((result) => {
+          res.sendStatus(result.status);
         });
     });
 
