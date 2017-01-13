bf2dba1f4fb0b1ba55e5297e0dd94b773fbf8b7f
Juraci Vieira <juraci.vieira@gmail.com>
2017-01-13 18:05:47 -0200

    adding ssl option

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,13 @@
 export default {
-  database: 'postgres',
+  database: process.env.DB_NAME || 'pomodorify',
   username: process.env.PG_USER || 'postgres',
   password: process.env.PG_PASSWORD || 'postgres',
   params: {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: !(process.env.NODE_ENV === 'test'),
+    },
     pool: {
       max: 5,
       min: 0,
