ce2f97c6892b166e3945282b5daabeb2f38ff098
Juraci Vieira <juraci.vieira@gmail.com>
2017-02-11 18:27:42 -0200

    refactoring UsersControllers

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -15,18 +15,26 @@ class UsersController extends ApplicationController {
   }
 
   findAll(query) {
-    return this.User.findAll({ where: query.filter, include: [{ model: this.Goal, as: 'goals' }] })
+    return this.User.findAll({
+      where: query.filter,
+      include: [
+        { model: this.Goal, as: 'goals' },
+      ],
+    })
       .then(users => this.serialize(users))
       .then(data => UsersController.ok(data))
       .catch(err => UsersController.unprocessableEntity(err));
   }
 
   findById(id) {
-    return this.User.find({ where: { id: parseInt(id, 10) }, include: [{ model: this.Goal, as: 'goals' }] })
-      .then((user) => {
-        if (!user) { throw new Error('user not found'); }
-        return this.serialize(user);
-      })
+    return this.User.find({
+      where: { id: parseInt(id, 10) },
+      include: [
+        { model: this.Goal, as: 'goals' },
+      ],
+    })
+      .then(user => UsersController.throwIfNotFound(user))
+      .then(user => this.serialize(user))
       .then(data => UsersController.ok(data))
       .catch(err => UsersController.notFound(err));
   }
@@ -34,10 +42,8 @@ class UsersController extends ApplicationController {
   updateById(id, user) {
     return this.deserialize(user)
       .then(dsUser => this.User.update(dsUser, { where: { id: parseInt(id, 10) } }))
-      .then((result) => {
-        if (result[0] !== 1) { throw new Error('user not found'); }
-        return UsersController.noContent();
-      })
+      .then(result => UsersController.throwIfNotUpdated(result))
+      .then(() => UsersController.noContent())
       .catch(err => UsersController.notFound(err));
   }
 }
