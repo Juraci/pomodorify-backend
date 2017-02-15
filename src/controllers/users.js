2fbed9356b2eabc2c281776a60d27a3b43c23d46
Juraci Vieira <juraci.vieira@gmail.com>
2017-02-15 13:58:46 -0200

    extracting findall from users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,22 +8,12 @@ class UsersController extends ApplicationController {
     super({
       serializer: userSerializer.buildSerializer(),
       deserializer: UserSerializer.buildDeserializer(),
+      model: models.User,
+      relations: [{ model: models.Goal, as: 'goals' }],
     });
 
     this.Goal = models.Goal;
     this.User = models.User;
-  }
-
-  findAll(query) {
-    return this.User.findAll({
-      where: query.filter,
-      include: [
-        { model: this.Goal, as: 'goals' },
-      ],
-    })
-      .then(users => this.serialize(users))
-      .then(data => UsersController.ok(data))
-      .catch(err => UsersController.unprocessableEntity(err));
   }
 
   findById(id) {
